fe58415595753fd7ddb9f19e9dc36a233f42e506
LiveIsAbsurd <anndreybykov19@gmail.com>
2025-09-17 23:51:35 +0300

    labuba

--- src/components/Place/Place.jsx
+++ src/components/Place/Place.jsx
@@ -3,9 +3,11 @@ import styles from "./Place.module.scss";
 import macValidator from "../../functions/macValidator";
 import macConvert from "../../functions/macConvert";
 import vendorAPI from "../../functions/vendorAPI";
+import labuba from "../../assets/labuba.png";
 
 const Place = () => {
     const [mac, setMac] = useState();
+    const [labubuPic, setLabubuPic] = useState(false);
     const valid = macValidator(mac);
     const [copied, setCopied] = useState(false);
     const [vendor, setVendor] = useState();
@@ -16,13 +18,21 @@ const Place = () => {
         setVendor(vendorAPI(mac));
     }
 
+    const setLabubu = (text) => {
+        if (text == "labubu") {
+            setLabubuPic(!labubuPic);
+        }
+    }
+
     return (
         <div className={styles.main}>
+            {labubuPic ? <img onClick={() => setLabubuPic(false)} className={styles.labuba} src={labuba}></img> : null}
             <div>Введите МАКъ</div>
             <input 
                 onChange={(e) => {
                     setMac(e.target.value.trim());
                     setVendor("");
+                    setLabubu(e.target.value.trim());
                 }} 
                 value={mac} type="text" className={`${styles.input} ${!valid && mac ? styles.input_alarm : ''}`} />
 
