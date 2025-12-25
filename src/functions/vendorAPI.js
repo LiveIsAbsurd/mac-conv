c589ddc501fa47f99f5acf03941379383c330516
LiveIsAbsurd <anndreybykov19@gmail.com>
2025-12-25 19:35:57 +0300

    новый api

--- src/functions/vendorAPI.js
+++ src/functions/vendorAPI.js
@@ -1,5 +1,5 @@
 const vendorAPI = async (mac) => {
-    const info = await fetch(`https://v3789.hosted-by-vdsina.com:3001/searchVendor/${mac}`);
+    const info = await fetch(`https://v3008618.hosted-by-vdsina.ru:3001/searchVendor/${mac}`);
     const infoMac = await info.json();
     return infoMac;
 }
