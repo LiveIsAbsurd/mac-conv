30adf9d26d727d1b966e9164a89aa77ff5edf7d0
LiveIsAbsurd <anndreybykov19@gmail.com>
2026-07-01 23:19:00 +0300

    кэш

--- src/functions/vendorAPI.js
+++ src/functions/vendorAPI.js
@@ -1,5 +1,11 @@
 const vendorAPI = async (mac) => {
-    const info = await fetch(`https://andruha-host.ru:3001/searchVendor/${mac}`);
+    const info = await fetch(`https://andruha-host.ru:3001/searchVendor/${mac}`, {
+        cache: 'no-store',
+        headers: {
+            'Cache-Control': 'no-cache'
+        }
+    });
+    
     const infoMac = await info.json();
     return infoMac;
 }
